973c32ab8daa70599cdf53e66fd637cd0956b87a
Adam Reece <adam@adamreecestudios.com>
2020-09-21 17:09:07 -0400

    Updates

---
+++ precache-manifest.186b28c2bdad2db70d5d665e8e961d13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1e11d0a46e581aa16b83b958dc5334",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7e7f17a2de56f13d7848",
+    "url": "/monsters-rolodex/static/css/main.fd783453.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7f17a2de56f13d7848",
+    "url": "/monsters-rolodex/static/js/main.0fe12d65.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
